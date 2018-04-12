06691501b9541e768baa9fdede71043ad67eb39c
azjhe <38303407+azjhe@users.noreply.github.com>
2018-04-12 10:47:14 +0800

    Add files via upload

---
+++ application/views/template/menu/admin_menu.php
@@ -0,0 +1,59 @@
+
+        <nav class="navbar navbar-inverse navbar-fixed-top">
+            <div class="container-fluid">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" data-target="#navbarCollapse" data-toggle="collapse" class="navbar-toggle">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="<?php echo base_url()?>home"><span class="glyphicon glyphicon-home"></span> Lodi Studio</a>
+            </div>
+            <!-- Collection of nav links, forms, and other content for toggling -->
+            <div id="navbarCollapse" class="collapse navbar-collapse">
+                <ul class="nav navbar-nav">
+                    <li class="dropdown">
+                      <a class="dropdown-toggle" data-toggle="dropdown" href="#">Page 1<span class="caret"></span></a>
+                      <ul class="dropdown-menu">
+                        <li><a href="#">Page 1-1</a></li>
+                        <li><a href="#">Page 1-2</a></li>
+                        <li><a href="#">Page 1-3</a></li>
+                      </ul>
+                    </li>
+                    <li class="dropdown">
+                      <a class="dropdown-toggle" data-toggle="dropdown" href="#">Page 2<span class="caret"></span></a>
+                      <ul class="dropdown-menu">
+                        <li><a href="#">Page 2-1</a></li>
+                        <li><a href="#">Page 2-2</a></li>
+                        <li><a href="#">Page 3-3</a></li>
+                      </ul>
+                    </li>
+                    <li class="dropdown">
+                      <a class="dropdown-toggle" data-toggle="dropdown" href="#">Page 3<span class="caret"></span></a>
+                      <ul class="dropdown-menu">
+                        <li><a href="#">Page 3-1</a></li>
+                        <li><a href="#">Page 3-2</a></li>
+                        <li><a href="#">Page 3-3</a></li>
+                      </ul>
+                    </li>
+                 </ul>
+                 <ul class="nav navbar-nav navbar-right">
+                   <li class="dropdown">
+                     <a class="dropdown-toggle" data-toggle="dropdown" href="#">
+                         <span class="glyphicon glyphicon-user"></span> Right-Menu <span class="caret"></span>
+                     </a>
+                     <ul class="dropdown-menu">
+                       <li><a href=""><span class="glyphicon glyphicon-lock"></span> Right-Menu 1</a></li>
+                       <li><a href=""><span class="glyphicon glyphicon-log-out"></span> Logout</a></li>
+                     </ul>
+                   </li>
+                 </ul>
+            </div>
+            </div>
+        </nav>
+        <br />
+        <br />
+        <br />
